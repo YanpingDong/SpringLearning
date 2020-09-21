fc5eac784429af55ba6fcc3dc3295d881ece7331
YanpingDong <lear521@163.com>
2020-09-21 23:10:41 +0800

    how to add menue by js source

--- DashboardDemo/source/dashboard/src/main/resources/static/js/sysIndex.js
+++ DashboardDemo/source/dashboard/src/main/resources/static/js/sysIndex.js
@@ -14,4 +14,41 @@
             $('#container').load(url);
 
         });
+
+
+
+
+
+        $.ajax({
+                  type: "GET",
+                  url: "/log/getLogList1",
+                  dataType : "json",
+                  contentType : "application/json",
+                  success: function (d) {
+                    console.log(d['data']);
+                    for(subData  in d['data']){
+                      for(detail in d['data'][subData])
+                      {
+                      let $menu = $('<a>',{
+                         'data-widget':"treeview",
+                         'data-accordion':"false",
+                         role:"menu",
+                         'class':"nav nav-pills nav-sidebar flex-column-1"
+                      });
+//                      let $menu = $("<ul data-widget=\"treeview\" data-accordion=\"false\" role=\"menu\" class=\"nav nav-pills nav-sidebar flex-column-1\"></>");
+
+                      let $menu_key = $('<a />',{
+                          href: "#",
+                          'class': "nav-link"
+                      });
+                      let menuename = d['data'][subData][detail]
+                      let $menue_p = $('<p />').append( menuename + "<i class=\"right fas fa-angle-left\"></i>")
+                         let abc = $menu_key.append($menue_p);
+                         let cba = $menu.append(abc);
+                         $("#dyptest").append(cba);
+                         console.log(d['data'][subData][detail]);
+                      }
+                    }
+                  }
+                });
     })
