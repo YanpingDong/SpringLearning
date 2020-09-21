65b75ea0ccaa3d5393a7dcf9b3899cff144e82c7
YanpingDong <lear521@163.com>
2020-09-21 16:51:36 +0800

    add log list page

---
+++ DashboardDemo/source/dashboard/src/main/java/com/dyp/dashboard/module/sys/ctrl/LogController.java
@@ -0,0 +1,77 @@
+package com.dyp.dashboard.module.sys.ctrl;
+
+import com.dyp.dashboard.module.sys.entity.SysLog;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping(value = "/log")
+public class LogController {
+    @RequestMapping(value = "/getLogList")
+    @ResponseBody
+    public Object getLogList(HttpServletRequest request)
+    {
+        int pageSize = 10;
+        try {
+            pageSize =  Integer.parseInt(request.getParameter("pageSize"));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        int pageNumber=0 ;
+        try {
+            pageNumber =  Integer.parseInt(request.getParameter("pageNumber"))-1;
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        Map<String, Object> map = new HashMap<>();
+
+        String searchText=request.getParameter("searchText")==null ? "": request.getParameter("searchText");
+
+        String sortName=request.getParameter("sortName")==null ? "roleId": request.getParameter("sortName");
+        String sortOrder=request.getParameter("sortOrder")==null ? "asc": request.getParameter("sortOrder");
+
+
+//        List<SysLog> sysLogPage =  logService.findAll(searchText);
+        List<SysLog>  sysLogs= new ArrayList<SysLog>();
+        SysLog sysLog1 = new SysLog();
+        sysLog1.setAction("GET");
+        sysLog1.setEvent("event");
+        sysLog1.setHost("localhost");
+        sysLog1.setInsertTime(LocalDateTime.now());
+        sysLog1.setLogId(1l);
+        sysLog1.setUserName("dyp");
+
+        SysLog sysLog2 = new SysLog();
+        sysLog2.setAction("GET");
+        sysLog2.setEvent("event2");
+        sysLog2.setHost("localhost2");
+        sysLog2.setInsertTime(LocalDateTime.now());
+        sysLog2.setLogId(2l);
+        sysLog2.setUserName("dyp2");
+        sysLogs.add(sysLog1);
+        sysLogs.add(sysLog2);
+        map.put("total",sysLogs.size());
+        map.put("rows",sysLogs);
+
+        return map;
+    }
+
+    @RequestMapping(value="/list")
+    public String list()
+    {
+        return "/sys/logList";
+    }
+}
