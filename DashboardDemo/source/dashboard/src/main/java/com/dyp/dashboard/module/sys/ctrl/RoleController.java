119eceb5f635092b2934ac81eb20a2289f5f948a
YanpingDong <lear521@163.com>
2020-09-23 21:49:57 +0800

    add all template page

---
+++ DashboardDemo/source/dashboard/src/main/java/com/dyp/dashboard/module/sys/ctrl/RoleController.java
@@ -0,0 +1,408 @@
+package com.dyp.dashboard.module.sys.ctrl;
+
+
+import com.dyp.dashboard.factory.LogFactory;
+import com.dyp.dashboard.module.sys.entity.SysLog;
+import com.dyp.dashboard.module.sys.entity.SysPermission;
+import com.dyp.dashboard.module.sys.entity.SysRole;
+import com.dyp.dashboard.module.sys.model.Pageable;
+import com.dyp.dashboard.module.sys.service.LogService;
+import com.dyp.dashboard.module.sys.service.RoleService;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@Controller
+@RequestMapping("/user")
+public class RoleController {
+    @Resource
+    RoleService roleService;
+    @Resource
+    LogService logService;
+
+    //,produces="application/json;charset=UTF-8"
+    @RequestMapping(value="/role")
+    @ResponseBody
+    @RequiresPermissions("role:view")
+    public Object getRole(HttpServletRequest request, HttpServletResponse response)
+    {
+
+        int pageSize = 10;
+        try {
+            pageSize =  Integer.parseInt(request.getParameter("pageSize"));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        int pageNumber=0 ;
+        try {
+            pageNumber =  Integer.parseInt(request.getParameter("pageNumber"))-1;
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        Map<String, Object> map = new HashMap<>();
+
+        String strRole=request.getParameter("searchText")==null ? "": request.getParameter("searchText");
+
+        String sortName=request.getParameter("sortName")==null ? "roleId": request.getParameter("sortName");
+        String sortOrder=request.getParameter("sortOrder")==null ? "asc": request.getParameter("sortOrder");
+
+        Pageable pageable = new Pageable(pageNumber,pageSize,"asc");
+        List<SysRole> sysRolePage = roleService.findAllByRoleContains(strRole, pageable);
+        map.put("total",sysRolePage.size());
+        map.put("rows",sysRolePage);
+
+        return map;
+
+
+    }
+
+    @RequestMapping("/rlist")
+    @RequiresPermissions("role:view")
+    public String list()
+    {
+        return "/sys/roleList";
+    }
+
+
+    @RequestMapping(value="/roleAdd", method= RequestMethod.GET)
+    @RequiresPermissions("role:add")
+    public String toAdd(SysRole sysRole) {
+//        sysRole.setAvailable(false);
+        return "/sys/roleAdd";
+    }
+
+    @RequestMapping(value="/roleAdd",method = RequestMethod.POST)
+    @ResponseBody
+    @RequiresPermissions("role:add")
+    public String save(SysRole sysRole, BindingResult bindingResult)
+    {
+        if(bindingResult.hasErrors())
+        {
+            return "0";
+        }
+        if(sysRole.getRoleId()==null)
+            sysRole.setCreateTime(LocalDateTime.now());
+        try {
+            roleService.save(sysRole);
+            SysLog sysLog = LogFactory.createSysLog("操作角色（新增或修改）","角色："+sysRole.getRole());
+            logService.writeLog(sysLog);
+
+            return  "/user/rlist";
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+            return "0";
+        }
+    }
+
+
+    @RequestMapping("/checkRoleExists")
+    @ResponseBody
+    public Object checkRoleExists(@RequestParam String newRole, @RequestParam(required = false) Integer roleId, @RequestParam(required = false) String oldRole)
+    {
+        Map<String,Boolean> map = new HashMap<>();
+        if(roleId==null)
+        {
+            boolean result = !roleService.checkRoleExists(newRole);
+            map.put("valid",result);
+        }
+        else
+        {
+            boolean result = !roleService.checkRoleExists(oldRole,newRole);
+            map.put("valid",result);
+        }
+        return map;
+    }
+
+
+    @RequestMapping(value = "/roleEdit/{id}")
+    @RequiresPermissions("role:edit")
+    public String edit(@PathVariable("id")Integer id, Map<String,Object> map)
+    {
+        SysRole sysRole = roleService.findById(id).orElse(new SysRole());
+        map.put("sysRole",sysRole);
+        return "/sys/roleAdd";
+    }
+    
+    @RequestMapping(value = "/roleDelete")
+    @ResponseBody
+    @RequiresPermissions("role:del")
+    public Object delete(@RequestParam String roleIdList)
+    {
+        if(roleIdList==null || roleIdList.isEmpty())
+        {
+            return "0";
+        }
+        String[] sList = roleIdList.split(",");
+        List<Integer> idList = new ArrayList<>();
+        for (String s:sList )
+        {
+            if(s.equalsIgnoreCase("1"))
+                return "0";
+            idList.add(Integer.parseInt(s));
+
+        }
+        boolean result = roleService.deleteAllByRoleIdIn(idList);
+        Map<String,String> map = new HashMap<>();
+        if(result)
+        {
+            map.put("success","true");
+            map.put("url","/user/rlist");
+
+            logService.writeLog(LogFactory.createSysLog("删除角色","角色id："+roleIdList));
+        }
+        else
+        {
+            map.put("error","true");
+        }
+
+        return map;
+    }
+
+    @RequestMapping(value = "/plist/{roleId}")
+    @RequiresPermissions("role:authorize")
+    public String permissionList(@PathVariable("roleId")Integer roleId, Model model)
+    {
+        SysRole sysRole = roleService.findById(roleId).orElse(new SysRole());
+
+        model.addAttribute("sysRole",sysRole);
+        return "/sys/sysPermission";
+    }
+
+    @RequestMapping("/getPermission/{roleId}")
+    @ResponseBody
+    @RequiresPermissions("role:authorize")
+    public Object getRolePermission(@PathVariable("roleId")Integer roleId)
+    {
+        if(roleId==null)
+            return null;
+
+        //以下数据应该是从数据库中取，但为了方便启动和演示就省了
+        SysPermission sysPermission1 = new SysPermission();
+        sysPermission1.setPermissionId(1);
+        sysPermission1.setAvailable(true);
+        sysPermission1.setPermissionName("系统管理");
+        sysPermission1.setParentId(0l);
+        sysPermission1.setParentIds("0");
+        sysPermission1.setPermission("system:view");
+        sysPermission1.setResourceType("menu");
+        sysPermission1.setUrl("#");
+        sysPermission1.setLevel(1);
+
+        SysPermission sysPermission2 = new SysPermission();
+        sysPermission2.setPermissionId(2);
+        sysPermission2.setAvailable(true);
+        sysPermission2.setPermissionName("用户管理");
+        sysPermission2.setParentId(1l);
+        sysPermission2.setParentIds("1");
+        sysPermission2.setPermission("user:view");
+        sysPermission2.setResourceType("menu");
+        sysPermission2.setUrl("user/userList");
+        sysPermission2.setLevel(2);
+
+        SysPermission sysPermission3 = new SysPermission();
+        sysPermission3.setPermissionId(3);
+        sysPermission3.setAvailable(true);
+        sysPermission3.setPermissionName("用户添加");
+        sysPermission3.setParentId(2l);
+        sysPermission3.setParentIds("1/2");
+        sysPermission3.setPermission("user:add");
+        sysPermission3.setResourceType("button");
+        sysPermission3.setUrl("user/userAdd");
+        sysPermission3.setLevel(3);
+
+
+        SysPermission sysPermission4 = new SysPermission();
+        sysPermission4.setPermissionId(4);
+        sysPermission4.setAvailable(true);
+        sysPermission4.setPermissionName("用户修改");
+        sysPermission4.setParentId(2l);
+        sysPermission4.setParentIds("1/2");
+        sysPermission4.setPermission("user:edit");
+        sysPermission4.setResourceType("button");
+        sysPermission4.setUrl("user/userEdit");
+        sysPermission4.setLevel(3);
+
+        SysPermission sysPermission5 = new SysPermission();
+        sysPermission5.setPermissionId(5);
+        sysPermission5.setAvailable(true);
+        sysPermission5.setPermissionName("用户删除");
+        sysPermission5.setParentId(2l);
+        sysPermission5.setParentIds("1/2");
+        sysPermission5.setPermission("user:del");
+        sysPermission5.setResourceType("button");
+        sysPermission5.setUrl("user/userDel");
+        sysPermission5.setLevel(3);
+
+        SysPermission sysPermission6 = new SysPermission();
+        sysPermission6.setPermissionId(6);
+        sysPermission6.setAvailable(true);
+        sysPermission6.setPermissionName("角色管理");
+        sysPermission6.setParentId(2l);
+        sysPermission6.setParentIds("1");
+        sysPermission6.setPermission("role:view");
+        sysPermission6.setResourceType("menu");
+        sysPermission6.setUrl("user/rList");
+        sysPermission6.setLevel(2);
+
+        SysPermission sysPermission7 = new SysPermission();
+        sysPermission7.setPermissionId(7);
+        sysPermission7.setAvailable(true);
+        sysPermission7.setPermissionName("角色添加");
+        sysPermission7.setParentId(6l);
+        sysPermission7.setParentIds("1/6");
+        sysPermission7.setPermission("role:add");
+        sysPermission7.setResourceType("button");
+        sysPermission7.setUrl("user/roleAdd");
+        sysPermission7.setLevel(3);
+
+        SysPermission sysPermission8 = new SysPermission();
+        sysPermission8.setPermissionId(8);
+        sysPermission8.setAvailable(true);
+        sysPermission8.setPermissionName("角色修改");
+        sysPermission8.setParentId(6l);
+        sysPermission8.setParentIds("1/6");
+        sysPermission8.setPermission("role:edit");
+        sysPermission8.setResourceType("button");
+        sysPermission8.setUrl("user/roleEdit");
+        sysPermission8.setLevel(3);
+
+        SysPermission sysPermission9 = new SysPermission();
+        sysPermission9.setPermissionId(9);
+        sysPermission9.setAvailable(true);
+        sysPermission9.setPermissionName("角色删除");
+        sysPermission9.setParentId(6l);
+        sysPermission9.setParentIds("1/6");
+        sysPermission9.setPermission("role:del");
+        sysPermission9.setResourceType("button");
+        sysPermission9.setUrl("user/roleDel");
+        sysPermission9.setLevel(3);
+
+        SysPermission sysPermission10 = new SysPermission();
+        sysPermission10.setPermissionId(10);
+        sysPermission10.setAvailable(true);
+        sysPermission10.setPermissionName("角色授权");
+        sysPermission10.setParentId(6l);
+        sysPermission10.setParentIds("1/6");
+        sysPermission10.setPermission("role:authorize");
+        sysPermission10.setResourceType("button");
+        sysPermission10.setUrl("user/authorize");
+        sysPermission10.setLevel(3);
+
+        SysPermission sysPermission13 = new SysPermission();
+        sysPermission13.setPermissionId(13);
+        sysPermission13.setAvailable(true);
+        sysPermission13.setPermissionName("角色授权");
+        sysPermission13.setParentId(6l);
+        sysPermission13.setParentIds("1/6");
+        sysPermission13.setPermission("role:authorize");
+        sysPermission13.setResourceType("button");
+        sysPermission13.setUrl("user/plist");
+        sysPermission13.setLevel(3);
+
+        //log:view
+        SysPermission sysPermission11 = new SysPermission();
+        sysPermission11.setPermissionId(11);
+        sysPermission11.setAvailable(true);
+        sysPermission11.setPermissionName("日志管理");
+        sysPermission11.setParentId(0l);
+        sysPermission11.setParentIds("0");
+        sysPermission11.setPermission("log:view");
+        sysPermission11.setResourceType("menu");
+        sysPermission11.setUrl("#");
+        sysPermission11.setLevel(1);
+
+        SysPermission sysPermission12 = new SysPermission();
+        sysPermission12.setPermissionId(12);
+        sysPermission12.setAvailable(true);
+        sysPermission12.setPermissionName("日志查看");
+        sysPermission12.setParentId(11l);
+        sysPermission12.setParentIds("11");
+        sysPermission12.setPermission("log:view");
+        sysPermission12.setResourceType("menu");
+        sysPermission12.setUrl("/log/list");
+        sysPermission12.setLevel(2);
+
+        List<SysPermission> sysPermissions = new ArrayList<>();
+
+
+        sysPermissions.add(sysPermission1);
+        sysPermissions.add(sysPermission2);
+        sysPermissions.add(sysPermission3);
+        sysPermissions.add(sysPermission4);
+        sysPermissions.add(sysPermission5);
+        sysPermissions.add(sysPermission6);
+        sysPermissions.add(sysPermission7);
+        sysPermissions.add(sysPermission8);
+        sysPermissions.add(sysPermission9);
+        sysPermissions.add(sysPermission10);
+        sysPermissions.add(sysPermission11);
+        sysPermissions.add(sysPermission12);
+
+
+
+        return sysPermissions;
+    }
+
+    @RequestMapping(value = "/toAuthorize")
+    @ResponseBody
+    @RequiresPermissions("role:authorize")
+    public Object toAuthorize(Integer roleId,String permissionIdList)
+    {
+        if(roleId==1) return 0;
+        Map<String,String> map = new HashMap<>();
+        if(permissionIdList==null || permissionIdList.isEmpty())
+        {
+            try {
+                roleService.clearAuthorization(roleId);
+                map.put("success","true");
+                map.put("url","/user/rlist");
+                logService.writeLog(LogFactory.createSysLog("清除角色权限","成功"));
+                return map;
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+                map.put("sucess","false");
+                map.put("url","/user/rlist");
+                return map;
+            }
+        }
+        String[] sList = permissionIdList.split(",");
+        List<Integer> idList = new ArrayList<>();
+        for (String s:sList )
+        {
+            idList.add(Integer.parseInt(s));
+        }
+
+        try {
+            roleService.grantAuthorization(roleId,idList);
+            map.put("sucess","true");
+            map.put("url","/user/rlist");
+            logService.writeLog(LogFactory.createSysLog("角色授权","权限列表："+permissionIdList));
+            return map;
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+            map.put("sucess","false");
+            map.put("url","/user/rlist");
+            return map;
+        }
+
+    }
+}
--- DashboardDemo/source/dashboard/src/main/java/com/dyp/dashboard/module/sys/ctrl/UserController.java
+++ DashboardDemo/source/dashboard/src/main/java/com/dyp/dashboard/module/sys/ctrl/UserController.java
@@ -1,8 +1,25 @@
 package com.dyp.dashboard.module.sys.ctrl;
 
+import com.dyp.dashboard.factory.LogFactory;
+import com.dyp.dashboard.module.sys.entity.SysUserRole;
+import com.dyp.dashboard.module.sys.entity.User;
+import com.dyp.dashboard.module.sys.model.Pageable;
+import com.dyp.dashboard.module.sys.service.LogService;
+import com.dyp.dashboard.module.sys.service.LoginService;
+import com.dyp.dashboard.module.sys.service.UserService;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: xu.dm
@@ -16,7 +33,12 @@ public class UserController {
     private int hashIterations = 2;
     private String salt = "8d78869f470951332959580424d4bf4f";
 
-
+    @Resource
+    UserService userService;
+    @Resource
+    LoginService loginService;
+    @Resource
+    LogService logService;
     /**
      * 用户查询.
      * @return
@@ -26,10 +48,334 @@ public class UserController {
         return "/sys/userList";
     }
 
+    @RequestMapping(value="/getUserList")
+    @RequiresPermissions("user:view")//权限管理;
+    @ResponseBody
+    public Object getUserList(HttpServletRequest request, HttpServletResponse response)
+    {
+        int pageSize = 10;
+        try {
+            pageSize =  Integer.parseInt(request.getParameter("pageSize"));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        int pageNumber=0 ;
+        try {
+            pageNumber =  Integer.parseInt(request.getParameter("pageNumber"))-1;
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        Map<String, Object> map = new HashMap<>();
+
+        String strUserName=request.getParameter("searchText")==null ? "": request.getParameter("searchText");
+
+        String sortName=request.getParameter("sortName")==null ? "roleId": request.getParameter("sortName");
+        String sortOrder=request.getParameter("sortOrder")==null ? "asc": request.getParameter("sortOrder");
+
+        Pageable pageable = new Pageable(pageNumber,pageSize,sortOrder.equalsIgnoreCase("asc")?"asc":"desc");
+        List<User> userPage = userService.findAllByUserNameContains(strUserName,pageable);
+
+        map.put("total",userPage.size());
+        map.put("rows",userPage);
+
+        return map;
+
+
+    }
 
     @RequestMapping(value = "/add",method = RequestMethod.GET)
-    public String toUserAdd(){
+    @RequiresPermissions("user:add")//权限管理;
+    public String toUserAdd(User user){
         return "/sys/userAdd";
+    }
+
+
+    /**
+     * 用户添加;
+     * @return
+     */
+    @RequestMapping(value = "/add",method = RequestMethod.POST)
+    @RequiresPermissions("user:add")//权限管理;
+    @ResponseBody
+    public String save(User user, BindingResult bindingResult, String password2){
+        if(bindingResult.hasErrors())
+        {
+            return "0";
+        }
+
+        user.setSalt(this.salt);
+        if(user.getUserId()==null) {
+            user.setCreateTime(LocalDateTime.now());
+//            String encryptPwd = EncryptUtils.encrypt(user.getPassword(),user.getCredentialsSalt(),this.algorithmName,this.hashIterations);
+            String  encryptPwd=user.getPassword();
+            user.setPassword(encryptPwd);
+        }
+        else {
+            if(!user.getPassword().equals(password2))
+            {
+//                String encryptPwd = EncryptUtils.encrypt(user.getPassword(),user.getCredentialsSalt(),this.algorithmName,this.hashIterations);
+                String  encryptPwd=user.getPassword();
+                user.setPassword(encryptPwd);
+            }
+        }
+        try {
+            userService.save(user);
+            logService.writeLog(LogFactory.createSysLog("新增或修改用户","用户："+user.getUserName()));
+            return "/user/ulist";
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+            return "0";
+        }
+    }
+
+    @RequestMapping("/checkUserExists")
+    @ResponseBody
+    public Object checkRoleExists(@RequestParam String newUserName, @RequestParam(required = false) Integer userId, @RequestParam(required = false) String oldUserName)
+    {
+        Map<String,Boolean> map = new HashMap<>();
+        if(userId==null)
+        {
+            boolean result = !userService.checkUserExists(newUserName);
+            map.put("valid",result);
+        }
+        else
+        {
+            boolean result = !userService.checkUserExists2(oldUserName,newUserName);
+            map.put("valid",result);
+        }
+        return map;
+    }
+
+
+    @RequestMapping(value = "/edit/{id}")
+    @RequiresPermissions("user:edit")
+    public String edit(@PathVariable("id")Integer id, Map<String,Object> map)
+    {
+        User user = userService.findUserById(id);
+        map.put("user",user);
+        return "/sys/userForm";
+    }
+
+    /**
+     * 用户删除;
+     * @return
+     */
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    @ResponseBody
+    @RequiresPermissions("user:del")//权限管理;
+    public Object userDel(@RequestParam String userIdList){
+        if(userIdList==null || userIdList.isEmpty())
+        {
+            return "0";
+        }
+        String[] sList = userIdList.split(",");
+        List<Integer> idList = new ArrayList<>();
+        for (String s:sList )
+        {
+            if(s.equalsIgnoreCase("1"))
+                return "0";
+            idList.add(Integer.parseInt(s));
+
+        }
+        Map<String,String> map = new HashMap<>();
+        try {
+            userService.deleteAllUserByUserIdList(idList);
+            map.put("success","true");
+            map.put("url","/user/ulist");
+            logService.writeLog(LogFactory.createSysLog("删除用户","用户id："+userIdList));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+            map.put("error","true");
+        }
+
+        return map;
+    }
+
+    @RequestMapping(value="/toListUserRole/{userId}")
+    @RequiresPermissions("user:roleGrant")
+    public String listUserRole(@PathVariable("userId")Integer userId, Map<String, Object> map)
+    {
+        User user = userService.findUserById(userId);
+        map.put("user",user);
+        return "/sys/userRole";
+    }
+
+
+    @RequestMapping(value = "/toGetUserRole/{userId}")
+    @ResponseBody
+    @RequiresPermissions("user:roleGrant")
+    public Object getUserRole(@PathVariable("userId")Integer userId)
+    {
+        if(userId==null)
+            return null;
+
+        List<SysUserRole> list = userService.findAllUserRoleByUserId(userId);
+        return list;
+    }
+
+
+
+
+    @RequestMapping(value="/toGrantUserRole")
+    @ResponseBody
+    @RequiresPermissions("user:roleGrant")
+    public Object grantUserRole(Integer userId, String roleIdList)
+    {
+        if(userId==1) return 0;
+        Map<String,String> map = new HashMap<>();
+        if(roleIdList==null || roleIdList.isEmpty())
+        {
+            try {
+                userService.deleteAllUserRoleByUserId(userId);
+                map.put("success","true");
+                map.put("url","/user/ulist");
+                logService.writeLog(LogFactory.createSysLog("角色清除","用户ID："+userId));
+                return map;
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+                map.put("sucess","false");
+                map.put("url","/user/ulist");
+                return map;
+            }
+        }
+        String[] sList = roleIdList.split(",");
+        List<Integer> idList = new ArrayList<>();
+        for (String s:sList )
+        {
+            idList.add(Integer.parseInt(s));
+        }
+
+        try {
+            userService.grantUserRole(userId,idList);
+            map.put("sucess","true");
+            map.put("url","/user/ulist");
+            logService.writeLog(LogFactory.createSysLog("角色分配","用户ID："+userId+" 角色id列表："+roleIdList));
+
+            return map;
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+            map.put("sucess","false");
+            map.put("url","/user/ulist");
+            return map;
+        }
+    }
+
+    @RequestMapping(value="/toChangePassword")
+    public String toChangePassword(String password,String newPassword,String newPassword2)
+    {
+        return "/sys/changePwd";
+    }
+
+    @RequestMapping(value="/changePassword",method = RequestMethod.POST)
+    @ResponseBody
+    public Object changePassword(HttpServletRequest request)
+    {
+        Map<String,String> map = new HashMap<>();
+        String password = request.getParameter("password");
+        String newPassword = request.getParameter("newPassword");
+        String newPassword2 = request.getParameter("newPassword2");
+
+        if(newPassword==null)
+        {
+            map.put("success","false");
+            map.put("result","密码不能为空");
+            return map;
+        }
+
+        if(!newPassword.equals(newPassword2))
+        {
+            map.put("success","false");
+            map.put("result","两次密码输入不一样");
+            return map;
+        }
+
+        if(newPassword.length()<6)
+        {
+            map.put("success","false");
+            map.put("result","密码长度必须大于6位");
+            return map;
+        }
+
+
+        String userName = loginService.getCurrentUserName();
+        if(userName==null || userName.isEmpty())
+        {
+            map.put("success","false");
+            map.put("result","用户错误");
+            return map;
+        }
+        User user = userService.findByUserName(userName);
+        if(user==null)
+        {
+            map.put("success","false");
+            map.put("result","用户错误");
+            map.put("url","/logout");
+            return map;
+        }
+
+//        String encryptPwd = EncryptUtils.encrypt(password,user.getCredentialsSalt(),this.algorithmName,this.hashIterations);
+        String encryptPwd = password;
+        if(!encryptPwd.equals(user.getPassword()))
+        {
+            map.put("success","false");
+            map.put("result","当前用户密码不正确");
+            map.put("url","/user/edit/1");
+            return map;
+        }
+
+//        String encryptNewPwd =EncryptUtils.encrypt(newPassword,user.getCredentialsSalt(),this.algorithmName,this.hashIterations);
+        String encryptNewPwd = password;
+        user.setPassword(encryptNewPwd);
+        userService.save(user);
+        map.put("success","true");
+        map.put("result","密码修改成功，请重新登录");
+        map.put("url","/logout");
+        logService.writeLog(LogFactory.createSysLog("修改密码","成功"));
+        return map;
+    }
+
+    @RequestMapping(value="/toCheckPwd")
+    @ResponseBody
+    public Object checkCurrentPwd(@RequestParam String password)
+    {
+        Map<String,Boolean> map = new HashMap<>();
+        if(password==null)
+        {
+            map.put("valid",true);
+            return map;
+        }
+
+        String userName = loginService.getCurrentUserName();
+        if(userName==null || userName.isEmpty())
+        {
+            map.put("valid",true);
+            return map;
+        }
+        User user = userService.findByUserName(userName);
+        if(user==null)
+        {
+            map.put("valid",true);
+            return map;
+        }
+
+//        String encryptPwd = EncryptUtils.encrypt(password,user.getCredentialsSalt(),this.algorithmName,this.hashIterations);
+        String encryptPwd = password;
+        if(!encryptPwd.equals(user.getPassword()))
+        {
+            map.put("valid",true);
+            return map;
+        }
+
+        map.put("valid",true);
+        return map;
     }
 
 
